729813fb54e22a03c47cbbb53cbc5175098e5b91
melisaaltinsoy <32497953+melisaaltinsoy@users.noreply.github.com>
2018-12-08 22:34:09 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,12 +5,14 @@ function setup() {
 }
 
 function draw() {
-  background(100);
+  background(175);
+  
   rectMode(CENTER);
   noStroke();
   fill(0,0,255);
   rotateX(angle);
   rotateY(angle * 0.3);
-  rect(0,0,80,80);
+  rotateZ(angle * 1.2);
+  rect(0,0,150,150);
   angle += 0.07;
 }

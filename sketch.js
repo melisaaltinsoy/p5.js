0e459405b38780dbd4480ad4c27e27ab771dfcb0
melisaaltinsoy <32497953+melisaaltinsoy@users.noreply.github.com>
2018-12-08 22:45:56 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ function draw() {
   fill(0,250,250);
   rotateX(angle);
   rotateY(angle * 0.3);
-  rotateZ(angle * 1.2);
+  //rotateZ(angle * 1.2);
   //rect(0,0,150,150);
   box(10,50,100);
   angle += 0.07;

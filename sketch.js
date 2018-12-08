1e2cc7f7097b246f8274137394a6b1a669871629
melisaaltinsoy <32497953+melisaaltinsoy@users.noreply.github.com>
2018-12-08 22:24:38 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+let angle=0;
+
 function setup() {
   createCanvas(400, 300, WEBGL);
 }
@@ -6,5 +8,7 @@ function draw() {
   background(100);
   rectMode(CENTER);
   noStroke();
+  rotateX(angle);
   rect(0,0,80,80);
+  angle += 0.07;
 }
